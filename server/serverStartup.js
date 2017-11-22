941378a39261aa9839d74738fe6ad6af661aa0bb
Henrik Kniberg <h@kniberg.com>
2017-11-22 10:42:32 +0700

    minor fix

--- server/serverStartup.js
+++ server/serverStartup.js
@@ -1,4 +1,11 @@
 import {attachSchemas} from "../lib/schemas/schemas";
 import {initDb} from "./initDb";
+
+if (process.env.MAIL_URL) {
+  console.log("Will use MAIL_URL " + process.env.MAIL_URL)
+} else {
+  console.log("Warning: MAIL_URL isn't set, so I can't send booking confirmation emails.")
+}
+
 attachSchemas()
 initDb()
