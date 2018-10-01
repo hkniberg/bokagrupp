8c3d1bfa1add614ec88d8f7b1b5a3f6661fa9644
Henrik Kniberg <h@kniberg.com>
2018-10-01 13:46:05 +0200

    Bug fixes

--- client/admin/attendance/attendance.js
+++ client/admin/attendance/attendance.js
@@ -1,20 +1,39 @@
 import {getAttendanceDatesForSlot} from "../../../lib/methods/attendanceMethods";
 import {wasAttended} from "../../../lib/methods/attendanceMethods";
+import {getBookingsThatHaveAttendedSlot} from "../../../lib/methods/attendanceMethods";
+import {getAdminRouteToOrg} from "../../../lib/router";
 Template.attendance.helpers({
   attendanceLink() {
     const course = Template.currentData()
     return Meteor.absoluteUrl("attendance/" + course.attendanceKey)
   },
-  
+
+  /*
+    All bookings currently associated to this slot,
+    Plus all bookings that have ever attended this slot.
+    Sorted by first name.
+   */
+  bookingsForSlot() {
+    const slot = Template.currentData()
+    const bookings = slot.bookings().fetch()
+    const bookingsThatHaveAttended = getBookingsThatHaveAttendedSlot(slot._id)
+    bookingsThatHaveAttended.forEach((bookingThatHasAttended) => {
+      if (bookingThatHasAttended.slotId != slot._id) {
+        bookingThatHasAttended.changedGroup = true
+        bookings.push(bookingThatHasAttended)
+      }
+    })
+    return bookings
+  },
+
   attendanceDatesForSlot() {
     const slot = Template.currentData()
     return getAttendanceDatesForSlot(slot)
   },
 
   attendanceDatesForBooking() {
-    const booking = Template.currentData()
-
-    return getAttendanceDatesForSlot(booking.slot())
+    const slot = Template.parentData()
+    return getAttendanceDatesForSlot(slot)
   },
   
   wasPresent() {
@@ -26,9 +45,10 @@ Template.attendance.helpers({
   attendanceCheckboxData() {
     const date = Template.currentData()
     const booking = Template.parentData()
+    const slot = Template.parentData(2)
     return {
       booking: booking,
-      slot: booking.slot(),
+      slot: slot,
       date: date
     }
   },
@@ -36,11 +56,26 @@ Template.attendance.helpers({
   attendanceCellCss() {
     const date = Template.currentData()
     const booking = Template.parentData()
-    const slot = booking.slot()
+    const slot = Template.parentData(2)
     if (wasAttended(booking._id, slot._id, date)) {
       return "present"
     } else {
       return "notPresent"
+    }
+  },
+
+  childNameCellCss() {
+    const booking = Template.currentData()
+    if (booking.changedGroup) {
+      return "grayText"
+    }
+  },
+
+  coursePath() {
+    const course = Template.currentData()
+    if (course) {
+      const orgPath = getAdminRouteToOrg(course.org())
+      return orgPath + "/course/" + course.shortName
     }
   }
 })
